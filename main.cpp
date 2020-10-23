628eb1c7998a2a0bb6f51946d4258e8b9097f616
smg-73 <sixtomg7@gmail.com>
2020-10-23 17:10:06 -0400

    added variables and stores user input values into variables for the fractions

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//main calculator function
+//ask user to input a number for numerator and denominatior for each fraction
+void Calculator()
+{
+
+    int n1, d1, n2, d2; //initializes variables used for fraction
+
+    //ask user to input number and stores it in respective variable
+    cout << "Enter Numerator for first fraction: ";
+    cin >> n1;
+
+    cout << "Enter Denominator for first fraction: ";
+    cin >> d1;
+
+    cout << "Enter Numerator for second fraction: ";
+    cin >> n2;
+
+    cout << "Enter Denominator for second fraction: ";
+    cin >> d2;
+}
+
+int main()
+{
+    return 0;
+}
